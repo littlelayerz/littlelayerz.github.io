5deb58767da0b7dae1d34b27fd91bda234f82e6d
Girish Sukinkar <kodegame0@gmail.com>
2026-06-13 23:08:09 +0530

    Fix back icon to use proper arrow-left SVG

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ function renderSingleProduct(product, container) {
   container.innerHTML = `
     <div class="back-nav">
       <a href="${window.PRODUCT_SLUG ? '../../' : './'}" class="back-link">
-        <svg class="icon" style="transform: rotate(180deg);" viewBox="0 0 24 24"><path d="M8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z"/></svg>
+        <svg class="icon" viewBox="0 0 24 24" fill="currentColor"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
         Back to Catalog
       </a>
     </div>
